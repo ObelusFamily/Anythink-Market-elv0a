b04eb1cf63fc3365eb1832fc84d9f39f7416f6c7
Nicolas de Moura <nicolasdmoura@192.168.15.81>
2022-09-24 15:59:20 -0300

    fix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 import defaultIMG from "../imgs/placeholder.png";
 
+
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
     dispatch({
